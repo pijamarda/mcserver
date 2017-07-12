a914916c78e2b0b0586d8ad76690ce6409dc82b4
zeneke <zeneke@gmail.com>
2017-07-12 14:10:47 +0200

    fixed string lookup

--- mcserversite/monitor/views.py
+++ mcserversite/monitor/views.py
@@ -9,8 +9,8 @@ from django.conf import settings
 def index(request):
     estado_server = 'apagado'
     estado_mc = 'apagado'
-    file_server = open(os.path.join(settings.BASE_DIR, 'static/status/server.txt'))
-    file_mc = open(os.path.join(settings.BASE_DIR, 'static/status/mc.txt'))
+    file_server = open(os.path.join(settings.BASE_DIR, 'static/status/server.txt')).read()
+    file_mc = open(os.path.join(settings.BASE_DIR, 'static/status/mc.txt')).read()
     if 'encendido' in file_server:
         estado_server = 'encendido'
     if 'encendido' in file_mc:
