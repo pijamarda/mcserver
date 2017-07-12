4267eb16b5b281839e28c797bdeb7ef6295877d8
zeneke <zeneke@gmail.com>
2017-07-12 11:34:31 +0200

    started AJAX configuration

--- mcserversite/monitor/views.py
+++ mcserversite/monitor/views.py
@@ -2,4 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 def index(request):
-    return render(request, 'monitor/index.html', {})
+    estado = 'apagado'
+    return render(request, 'monitor/index.html', {'estado': estado})
+
+def encender(request):    
+    if request.method == 'GET':
+                print('encender')
+    response = 'ok'
+    return HttpResponse(response)
