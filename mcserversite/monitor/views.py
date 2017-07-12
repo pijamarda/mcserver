e21e02c59dfea5e421ca0251f1e866aba585d162
zeneke <zeneke@gmail.com>
2017-07-12 13:34:44 +0200

    Now the button for turning on is working

--- mcserversite/monitor/views.py
+++ mcserversite/monitor/views.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
@@ -6,12 +7,22 @@ from django.templatetags.static import static
 from django.conf import settings
 
 def index(request):
-    estado = 'apagado'
+    estado_server = 'apagado'
+    estado_mc = 'apagado'
     file_server = open(os.path.join(settings.BASE_DIR, 'static/status/server.txt'))
-    file_mcserver = open(os.path.join(settings.BASE_DIR, 'static/status/mcserver.txt'))
-    return render(request, 'monitor/index.html', {'estado': estado})
+    file_mc = open(os.path.join(settings.BASE_DIR, 'static/status/mc.txt'))
+    if 'encendido' in file_server:
+        estado_server = 'encendido'
+    if 'encendido' in file_mc:
+        estado_mc = 'encendido'
+    return render(request, 'monitor/index.html', {'estado_mc': estado_mc, 
+                                                  'estado_server': estado_server, 
+                                                  })
 
-def encender(request):    
+def encender(request):
+    script = os.path.join(settings.BASE_DIR, 'static/scripts/wakeonlan.sh')    
+    output = subprocess.call([script], shell = True)
+    print(output)
     if request.method == 'GET':
                 print('encender')
     response = 'ok'
---
+++ mcserversite/static/scripts/wakeonlan.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Arrancando la NIC1 del ESXi"
+wakeonlan 14:cc:20:04:81:fe
