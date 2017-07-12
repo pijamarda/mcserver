7fc58ef58d1553f6ad84f107241a311695bfa085
zeneke <zeneke@gmail.com>
2017-07-12 12:05:36 +0200

    added status files

--- mcserversite/monitor/views.py
+++ mcserversite/monitor/views.py
@@ -1,8 +1,14 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.templatetags.static import static
+from django.conf import settings
 
 def index(request):
     estado = 'apagado'
+    file_server = open(os.path.join(settings.BASE_DIR, 'static/status/server.txt'))
+    file_mcserver = open(os.path.join(settings.BASE_DIR, 'static/status/mcserver.txt'))
     return render(request, 'monitor/index.html', {'estado': estado})
 
 def encender(request):    
