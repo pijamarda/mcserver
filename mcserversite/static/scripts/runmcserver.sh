cd43ee74b6735490749ae33d29148c08c5c2839b
zeneke@raspberry2 <zeneke@gmail.com>
2017-07-17 11:46:52 +0200

    now the scripts runs with an specific SSH key

--- mcserversite/static/scripts/runmcserver.sh
+++ mcserversite/static/scripts/runmcserver.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Me conecto al servidor windows por SSH y ejecuto el BAT"
-ssh zeneke@10.0.0.40 'c:\users\zeneke\mcscripts\runserver.bat'
+#ssh -v zeneke@10.0.0.40 'c:\users\zeneke\mcscripts\runserver.bat'
+ssh -i /home/zeneke/.ssh/id_rsa_windows zeneke@10.0.0.40 'c:\users\zeneke\mcscripts\runserver.bat'
